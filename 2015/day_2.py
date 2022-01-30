d026b46f32e4f14040475ce9a9d3a1bf7a817ee0
Thomas Adams <thomas.adams@hutton.ac.uk>
2022-01-30 22:34:21 +0000

    Adds commands to calculate total area of paper requried

--- 2015/day_2.py
+++ 2015/day_2.py
@@ -14,3 +14,21 @@ def parse_args():
     parser.add_argument('--input', required=True,
                         help='Input file containing present dimensions')
     return parser.parse_args()
+
+# Calculate total area of paper needed
+
+
+def total_paper(input: list):
+    packages = []
+    for line in input:
+        line = line.rstrip()
+        dimensions = line.split('x')
+        area_1 = dimensions[0] * dimensions[1]
+        area_2 = dimensions[0] * dimensions[2]
+        area_3 = dimensions[1] * dimensions[2]
+        areas_list = [area_1, area_2, area_3]
+        areas_list.sort()
+        package_area = (2 * area_1) + (2 * area_2) + (2 * area_3) +
+        areas_list[0]
+        packages.append(package_area)
+    total_area = sum(packages)
