1ead14aeb8acedb84552f36aaf5fc8ee2c5c8cd4
Thomas Adams <thomas.adams@hutton.ac.uk>
2022-01-30 21:35:14 +0000

    Fixes formatting in parse_args

--- 2015/day_1.py
+++ 2015/day_1.py
@@ -10,8 +10,8 @@ import argparse
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description=
-                                     'Find out which floor Santa ends up on')
+    parser = argparse.ArgumentParser(description='Find out which floor Santa \
+    ends up on')
     parser.add_argument('--input', required=True,
                         help='Input file containing coded instructions')
     return parser.parse_args()
