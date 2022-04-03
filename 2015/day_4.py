0a37988ee16aa4d6e6fff65bb86a619e4c504fd9
Thomas Adams <thomas.adams@hutton.ac.uk>
2022-04-03 18:44:44 +0100

    Adds commands to test for the starting number

--- 2015/day_4.py
+++ 2015/day_4.py
@@ -11,7 +11,20 @@ import argparse
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Mine some advent coins')
+    parser = argparse.ArgumentParser(description='Mine some AdventCoins')
     parser.add_argument('--input', required=True,
                         help='Input secret key')
     return parser.parse_args()
+
+# Mine the advent coin
+
+
+def mine_coin(input: str):
+    number = 0
+    while True:
+        number += 1
+        string = input + str(number)
+        hash = hashlib.md5(string.encode('utf-8')).hexdigest()
+        if hash.startswith('00000'):
+            break
+    return str(number) + ' is the solution needed to mine an AdventCoin'
