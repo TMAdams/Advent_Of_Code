cb9d0ca6323d9f1af7433bd01910cf31f100def9
Thomas Adams <thomas.adams@hutton.ac.uk>
2022-04-03 17:07:01 +0100

    Adds commands to define function to parse CLI arguments

--- 2015/day_3.py
+++ 2015/day_3.py
@@ -5,3 +5,12 @@
 # Import python modules
 
 import argparse
+
+# Prepare function to parse CLI arguments
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Help Santa navigate')
+    parser.add_argument('--input', required=True,
+                        help='Input file containing directions')
+    return parser.parse_args()
