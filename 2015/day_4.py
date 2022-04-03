56b3e2f95a1371c012f44809b820337f10d33e94
Thomas Adams <thomas.adams@hutton.ac.uk>
2022-04-03 18:34:28 +0100

    Adds commands to parse CLI arguments

--- 2015/day_4.py
+++ 2015/day_4.py
@@ -6,3 +6,12 @@
 
 import hashlib
 import argparse
+
+# Prepare function to parse CLI arguments
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Mine some advent coins')
+    parser.add_argument('--input', required=True,
+                        help='Input secret key')
+    return parser.parse_args()
