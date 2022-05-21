5a585fbf322fd30909aa1abd3d89009c81f536a7
Thomas Adams <thomas.adams@hutton.ac.uk>
2022-05-21 18:16:46 +0100

    Adds commands to parse CLI arguments

--- 2015/day_5.py
+++ 2015/day_5.py
@@ -5,3 +5,13 @@
 # Import python modules
 
 import argparse
+
+# Prepare function to parse CLI arguments
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Find out who is naughty \
+    or nice')
+    parser.add_argument('--input', required=True,
+                        help='Input strings')
+    return parser.parse_args()
